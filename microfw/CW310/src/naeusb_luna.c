b5d0c348f6b8fa0217bf9ab12f658f2a86833376
Alex Dewar <alexldewar@gmail.com>
2023-02-07 15:47:07 -0400

    luna.c

--- microfw/CW310/src/naeusb_luna.c
+++ microfw/CW310/src/naeusb_luna.c
@@ -1,9 +1,19 @@
 #include "naeusb.h"
 #include "naeusb_luna.h"
 #include "naeusb_default.h"
+#include "fpga_program.h"
+#include "fpga_selectmap.h"
 
-uint8_t FPGA_MODE_PIN_STATE = 0b111;
 uint32_t FPGA_MODE_PINS[] = {PIN_FPGA_M0, PIN_FPGA_M1, PIN_FPGA_M2};
+
+uint8_t FPGA_CURRENT_PROG_MODE = 0;
+
+enum FPGA_PROG_MODE {
+    FPGA_PROG_MODE_DETECT,
+    FPGA_PROG_MODE_SERIAL,
+    FPGA_PROG_MODE_PARALLEL,
+    FPGA_PROG_MODE_UNKNOWN
+};
 
 void set_mode_pins(void)
 {
@@ -17,10 +27,48 @@ void set_mode_pins(void)
     }
 }
 
+uint8_t detect_programming_mode(void)
+{
+    uint8_t pin_vals = 0;
+    gpio_pin_is_high(PIN_FPGA_M0);
+    for (uint8_t i = 0; i < 3; i++) {
+        pin_vals |= gpio_pin_is_high(FPGA_MODE_PINS[i]) << i;
+    }
+
+    if (pin_vals == 0b111) {
+        return FPGA_PROG_MODE_SERIAL;
+    } else if (pin_vals == 0b110) {
+        return FPGA_PROG_MODE_PARALLEL;
+    }
+    return FPGA_PROG_MODE_UNKNOWN;
+
+}
+
+void luna_progfpga_bulk(void) {
+    uint32_t prog_freq = 20E6;
+    // uint8_t prog_mode = (udd_g_ctrlreq.wValue >> 8) & 0xFF;
+    switch (udd_g_ctrlreq.wValue & 0xFF) {
+        case 0xA0:
+            FPGA_CURRENT_PROG_MODE = (udd_g_ctrlreq.wValue >> 8) & 0xFF;
+            if (FPGA_CURRENT_PROG_MODE == FPGA_PROG_MODE_DETECT) {
+                FPGA_CURRENT_PROG_MODE = detect_programming_mode();
+            }
+            if (udd_g_ctrlreq.req.wLength == 4) {
+                prog_freq = *(CTRLBUFFER_WORDPTR);
+            }
+
+            if (FPGA_CURRENT_PROG_MODE == FPGA_PROG_MODE_SERIAL)
+                fpga_program_setup1(prog_freq);	
+            else if (FPGA_CURRENT_PROG_MODE == FPGA_PROG_MODE_PARALLEL)
+
+            break;
+    }
+}
+
 bool luna_setup_out_received(void)
 {
 	switch (udd_g_ctrlreq.req.bRequest) {
-        case REQ_FPGA_MODE_PINS:
+        case REQ_FPGA_PROGRAM:
             udd_g_ctrlreq.callback = set_mode_pins;
             return true;
     }
